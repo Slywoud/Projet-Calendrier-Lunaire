7a68b4b9b4910765451218ff5caea2f389416335
Slywoud <he202160@students.ephec.be>
2023-12-01 16:33:58 +0100

    Update nouelleinterface.py
    Adjustments to the sliders to allow the sliders to work together on the image.

--- nouelleinterface.py
+++ nouelleinterface.py
@@ -7,15 +7,12 @@ class ImageViewer:
         self.root = root
         self.root.title("Image Viewer")
 
-        # Créer un bouton pour ouvrir l'explorateur de fichiers
         self.open_button = tk.Button(root, text="Ouvrir une image", command=self.open_image)
         self.open_button.pack(pady=10)
 
-        # Zone d'affichage de l'image
         self.image_label = tk.Label(root)
         self.image_label.pack(pady=10)
 
-        # Curseurs pour ajuster la luminosité, le contraste et l'exposition
         self.brightness_scale = tk.Scale(root, label="Luminosité", from_=0, to=2, resolution=0.1, orient=tk.HORIZONTAL, command=self.update_brightness)
         self.brightness_scale.set(1.0)
         self.brightness_scale.pack(pady=5)
@@ -28,66 +25,102 @@ class ImageViewer:
         self.exposure_scale.set(1.0)
         self.exposure_scale.pack(pady=5)
 
+        self.current_brightness = 1.0
+        self.current_contrast = 1.0
+        self.current_exposure = 1.0
+
+        # Stocker les dernières valeurs des curseurs
+        self.last_brightness_value = 1.0
+        self.last_contrast_value = 1.0
+        self.last_exposure_value = 1.0
+
     def open_image(self):
-        # Ouvrir l'explorateur de fichiers
         file_path = filedialog.askopenfilename(filetypes=[("Images", "*.png;*.jpg;*.jpeg;*.gif;*.bmp")])
 
         if file_path:
-            # Charger l'image sélectionnée
             self.original_image = Image.open(file_path)
-
-            # Redimensionner l'image sans déformer (maintient le ratio d'aspect)
-            self.original_image.thumbnail((500, 500))  # Ajustez la taille maximale selon vos besoins
-
-            # Convertir l'image en un format compatible avec Tkinter
+            self.original_image.thumbnail((500, 500))
             self.tk_image = ImageTk.PhotoImage(self.original_image)
-
-            # Mettre à jour l'étiquette avec la nouvelle image
             self.image_label.config(image=self.tk_image)
-            self.image_label.image = self.tk_image  # Gardez une référence à l'image pour éviter la collecte par le garbage collector
+            self.image_label.image = self.tk_image
 
-            # Mettre à jour les curseurs
-            self.brightness_scale.set(1.0)
-            self.contrast_scale.set(1.0)
-            self.exposure_scale.set(1.0)
+            self.brightness_scale.set(self.current_brightness)
+            self.contrast_scale.set(self.current_contrast)
+            self.exposure_scale.set(self.current_exposure)
 
     def update_brightness(self, *args):
-        # Obtenir la valeur de la jauge de luminosité
         brightness_value = self.brightness_scale.get()
 
-        # Appliquer l'ajustement à l'image
-        enhanced_image = ImageEnhance.Brightness(self.original_image).enhance(brightness_value)
+        # Vérifier si tous les curseurs sont à leur valeur minimale
+        if brightness_value == 1.0 and self.contrast_scale.get() == 1.0 and self.exposure_scale.get() == 1.0:
+            enhanced_image = self.original_image
+        else:
+            # Mettre à jour les valeurs actuelles des curseurs
+            self.current_brightness = self.last_brightness_value * brightness_value
+            self.current_contrast = self.current_contrast / self.last_brightness_value * brightness_value
+            self.current_exposure = self.current_exposure / self.last_brightness_value * brightness_value
 
-        # Mettre à jour l'affichage
+            # Stocker la dernière valeur réelle du curseur
+            self.last_brightness_value = brightness_value
+
+            enhanced_image = self.apply_adjustments()
+
         self.update_display(enhanced_image)
 
     def update_contrast(self, *args):
-        # Obtenir la valeur de la jauge de contraste
         contrast_value = self.contrast_scale.get()
 
-        # Appliquer l'ajustement à l'image
-        enhanced_image = ImageEnhance.Contrast(self.original_image).enhance(contrast_value)
+        # Vérifier si tous les curseurs sont à leur valeur minimale
+        if self.brightness_scale.get() == 1.0 and contrast_value == 1.0 and self.exposure_scale.get() == 1.0:
+            enhanced_image = self.original_image
+        else:
+            # Mettre à jour les valeurs actuelles des curseurs
+            self.current_brightness = self.current_brightness / self.last_contrast_value * contrast_value
+            self.current_contrast = self.last_contrast_value * contrast_value
+            self.current_exposure = self.current_exposure / self.last_contrast_value * contrast_value
 
-        # Mettre à jour l'affichage
+            # Stocker la dernière valeur réelle du curseur
+            self.last_contrast_value = contrast_value
+
+            enhanced_image = self.apply_adjustments()
+
         self.update_display(enhanced_image)
 
     def update_exposure(self, *args):
-        # Obtenir la valeur de la jauge d'exposition
         exposure_value = self.exposure_scale.get()
 
-        # Appliquer l'ajustement à l'image
-        enhanced_image = ImageEnhance.Color(self.original_image).enhance(exposure_value)
+        # Vérifier si tous les curseurs sont à leur valeur minimale
+        if self.brightness_scale.get() == 1.0 and self.contrast_scale.get() == 1.0 and exposure_value == 1.0:
+            enhanced_image = self.original_image
+        else:
+            # Mettre à jour les valeurs actuelles des curseurs
+            self.current_brightness = self.current_brightness / self.last_exposure_value * exposure_value
+            self.current_contrast = self.current_contrast / self.last_exposure_value * exposure_value
+            self.current_exposure = self.last_exposure_value * exposure_value
 
-        # Mettre à jour l'affichage
+            # Stocker la dernière valeur réelle du curseur
+            self.last_exposure_value = exposure_value
+
+            enhanced_image = self.apply_adjustments()
+
         self.update_display(enhanced_image)
 
-    def update_display(self, enhanced_image):
-        # Convertir l'image pour Tkinter
-        tk_image = ImageTk.PhotoImage(enhanced_image)
+    def apply_adjustments(self):
+        brightness_value = self.current_brightness
+        contrast_value = self.current_contrast
+        exposure_value = self.current_exposure
 
-        # Mettre à jour l'étiquette avec la nouvelle image
+        enhanced_image = ImageEnhance.Brightness(self.original_image).enhance(brightness_value)
+        enhanced_image = ImageEnhance.Contrast(enhanced_image).enhance(contrast_value)
+        enhanced_image = ImageEnhance.Color(enhanced_image).enhance(exposure_value)
+
+        return enhanced_image
+
+    def update_display(self, enhanced_image):
+        tk_image = ImageTk.PhotoImage(enhanced_image)
         self.image_label.config(image=tk_image)
-        self.image_label.image = tk_image  # Gardez une référence à l'image pour éviter la collecte par le garbage collector
+        self.image_label.image = tk_image
+
 
 # Créer une instance de la classe ImageViewer
 root = tk.Tk()
