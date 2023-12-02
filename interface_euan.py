bd6a3c9b5d6b8a6d8be93b32eb2a605f986c03af
e.cockburn@students.ephec.be <116933854+Euan-lc@users.noreply.github.com>
2023-12-02 17:53:57 +0100

    so lonely

--- interface_euan.py
+++ interface_euan.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
+import tkinter.ttk as ttk
+from tkinter.constants import *
 from PIL import Image, ImageTk, ImageEnhance
 from tools import *
 
@@ -11,25 +13,33 @@ class ImageViewer:
         self.original_image = ''
         self.current_image = ''
 
-        # Zone d'affichage de l'image
-        self.image_label = tk.Label(self.root, text='image goes here')
-        self.image_label.grid(row=0, column=1, rowspan=6)
+        self.steps_frame = tk.Frame(self.root)
+        self.steps_frame.grid(row=0, column=0)
 
         self.scale_frame = tk.Frame(self.root)
-        self.scale_frame.grid(row=1, column=0, rowspan=4)
+        self.scale_frame.grid(row=1, column=0, rowspan=1)
+
+        self.image_label = tk.Label(self.root, text='image goes here')
+        self.image_label.grid(row=0, column=1, rowspan=2)
+
+        # self.scrollable_history = VerticalScrolledFrame(root)
+        # self.scrollable_history.grid(row=0, column=2, rowspan=2)
 
         # Curseurs pour ajuster la luminosité, le contraste et l'exposition
-        self.brightness_scale = tk.Scale(self.scale_frame, label="Luminosité", from_=0, to=2, resolution=0.1, orient=tk.HORIZONTAL,
+        self.brightness_scale = tk.Scale(self.scale_frame, label="Luminosité", from_=0, to=2, resolution=0.1,
+                                         orient=tk.HORIZONTAL,
                                          command=self.update_brightness)
         self.brightness_scale.set(1.0)
         self.brightness_scale.grid(row=0, column=0)
 
-        self.contrast_scale = tk.Scale(self.scale_frame, label="Contraste", from_=-255, to=255, resolution=1, orient=tk.HORIZONTAL,
+        self.contrast_scale = tk.Scale(self.scale_frame, label="Contraste", from_=-255, to=255, resolution=1,
+                                       orient=tk.HORIZONTAL,
                                        command=self.update_contrast)
         self.contrast_scale.set(1.0)
         self.contrast_scale.grid(row=1, column=0)
 
-        self.exposure_scale = tk.Scale(self.scale_frame, label="Exposition", from_=0.1, to=2, resolution=0.1, orient=tk.HORIZONTAL,
+        self.exposure_scale = tk.Scale(self.scale_frame, label="Exposition", from_=0.1, to=2, resolution=0.1,
+                                       orient=tk.HORIZONTAL,
                                        command=self.update_exposure)
         self.exposure_scale.set(1.0)
         self.exposure_scale.grid(row=2, column=0)
@@ -42,14 +52,12 @@ class ImageViewer:
         self.steps = [{'text': 'load Image', 'command': self.open_image, 'image': ''},
                       {'text': 'find moon', 'command': self.find_moon_handler, 'image': ''},
                       {'text': 'crop to moon', 'command': self.crop_handler, 'image': ''},
-                      {'text': 'binarize', 'command': self.find_moon_handler, 'image': ''},
+                      {'text': 'binarize', 'command': self.binarize_handler, 'image': ''},
                       {'text': 'straighten', 'command': self.straighten_handler, 'image': ''},
                       {'text': 'compare', 'command': self.compare_handler, 'image': ''}]
 
         self.stepIndex = 0
 
-        self.steps_frame = tk.Frame(self.root)
-        self.steps_frame.grid(row=0, column=0)
 
         self.step_button = tk.Button(self.steps_frame, text=self.steps[0]['text'], command=self.steps[0]['command'])
         self.step_button.grid(row=0, column=0, columnspan=2, pady=5)
@@ -62,6 +70,7 @@ class ImageViewer:
 
         root.columnconfigure(0, weight=1)
         root.columnconfigure(1, weight=3)
+        root.columnconfigure(2, weight=1)
 
     def next_step_handler(self):
         num_steps = len(self.steps)
@@ -74,7 +83,8 @@ class ImageViewer:
             self.stepIndex += 1
             if self.stepIndex == (num_steps - 1):
                 self.step_plus.config(bg='gray')
-            self.step_button.config(text=self.steps[self.stepIndex]['text'], command=self.steps[self.stepIndex]['command'])
+            self.step_button.config(text=self.steps[self.stepIndex]['text'],
+                                    command=self.steps[self.stepIndex]['command'])
             if self.steps[self.stepIndex]['image']:
                 print(f'setting image to {self.stepIndex}')
                 self.update_display(self.steps[self.stepIndex]['image'])
@@ -93,7 +103,8 @@ class ImageViewer:
             self.stepIndex -= 1
             if self.stepIndex == 0:
                 self.step_minus.config(bg='gray')
-            self.step_button.config(text=self.steps[self.stepIndex]['text'], command=self.steps[self.stepIndex]['command'])
+            self.step_button.config(text=self.steps[self.stepIndex]['text'],
+                                    command=self.steps[self.stepIndex]['command'])
             if self.steps[self.stepIndex]['image']:
                 print(f'setting image to {self.stepIndex}')
                 self.update_display(self.steps[self.stepIndex]['image'])
@@ -132,12 +143,23 @@ class ImageViewer:
             # Charger l'image sélectionnée
             self.original_image = Image.open(file_path)
             self.current_image = self.original_image
+
+            # Reinitialiser l'historique d'image
+            for step in self.steps:
+                step['image'] = ''
+
+            # Afficher l'image
             self.update_display(self.original_image)
 
             # Mettre à jour les curseurs
             self.brightness_scale.set(1.0)
             self.contrast_scale.set(1.0)
             self.exposure_scale.set(1.0)
+
+    def binarize_handler(self):
+        img = np.array(self.current_image)
+        binarized = binarize_image(img, thresh_value=self.binary_threshold_scale.get())
+        self.update_display(Image.fromarray(binarized))
 
     def update_brightness(self, *args):
         pass
@@ -165,9 +187,12 @@ class ImageViewer:
         # Convertir l'image pour Tkinter
         self.tk_image = ImageTk.PhotoImage(to_display)
 
-        # Mettre à jour l'étiquette avec la nouvelle image
         self.image_label.config(image=self.tk_image)
-
+        # for step in self.steps:
+        #     if step['image']:
+        #         to_pack = step['image'].copy()
+        #         to_pack.thumbnail((150, 150))
+        #         tk.Label(self.scrollable_history, image=ImageTk.PhotoImage(to_pack)).pack()
 
 class Result(tk.Frame):
     def __init__(self, parent, original_image, cropped_image, phase_image, phase_name):
@@ -190,6 +215,53 @@ class Result(tk.Frame):
 
         self.phase_name_label = tk.Label(self, text=phase_name)
         self.phase_name_label.grid(row=2, column=0, columnspan=2)
+
+
+class VerticalScrolledFrame(ttk.Frame):
+    """A pure Tkinter scrollable frame that actually works!
+    * Use the 'interior' attribute to place widgets inside the scrollable frame.
+    * Construct and pack/place/grid normally.
+    * This frame only allows vertical scrolling.
+    """
+
+    def __init__(self, parent, *args, **kw):
+        ttk.Frame.__init__(self, parent, *args, **kw)
+
+        # Create a canvas object and a vertical scrollbar for scrolling it.
+        vscrollbar = ttk.Scrollbar(self, orient=VERTICAL)
+        vscrollbar.pack(fill=Y, side=RIGHT, expand=FALSE)
+        canvas = tk.Canvas(self, bd=0, highlightthickness=0,
+                           yscrollcommand=vscrollbar.set)
+        canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+        vscrollbar.config(command=canvas.yview)
+
+        # Reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # Create a frame inside the canvas which will be scrolled with it.
+        self.interior = interior = ttk.Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=NW)
+
+        # Track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar.
+        def _configure_interior(event):
+            # Update the scrollbars to match the size of the inner frame.
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # Update the canvas's width to fit the inner frame.
+                canvas.config(width=interior.winfo_reqwidth())
+
+        interior.bind('<Configure>', _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # Update the inner frame's width to fill the canvas.
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+
+        canvas.bind('<Configure>', _configure_canvas)
 
 
 # Créer une instance de la classe ImageViewer
--- test.py
+++ test.py
@@ -1,58 +1,74 @@
-import numpy as np
-import cv2
-from tkinter import filedialog
-import tkinter as tk
+try:  # Python 2
+    import tkinter as tk
+    import tkinter.ttk as ttk
+    from tkinter.constants import *
+except ImportError:  # Python 2
+    import Tkinter as tk
+    import ttk
+    from tkinter.constants import *
 
 
-def moon_coordinates(img):
-    contrast_value = 100
+# Based on
+#   https://web.archive.org/web/20170514022131id_/http://tkinter.unpythonic.net/wiki/VerticalScrolledFrame
 
-    image = cv2.imread(img, cv2.IMREAD_COLOR)
+class VerticalScrolledFrame(ttk.Frame):
+    """A pure Tkinter scrollable frame that actually works!
+    * Use the 'interior' attribute to place widgets inside the scrollable frame.
+    * Construct and pack/place/grid normally.
+    * This frame only allows vertical scrolling.
+    """
+    def __init__(self, parent, *args, **kw):
+        ttk.Frame.__init__(self, parent, *args, **kw)
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Create a canvas object and a vertical scrollbar for scrolling it.
+        vscrollbar = ttk.Scrollbar(self, orient=VERTICAL)
+        vscrollbar.pack(fill=Y, side=RIGHT, expand=FALSE)
+        canvas = tk.Canvas(self, bd=0, highlightthickness=0,
+                           yscrollcommand=vscrollbar.set)
+        canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+        vscrollbar.config(command=canvas.yview)
 
-    # Réduction du bruit
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        # Reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
 
-    # Détermination d'un seuil adaptatif
-    _, contrast = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # Create a frame inside the canvas which will be scrolled with it.
+        self.interior = interior = ttk.Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=NW)
 
-    circles = cv2.HoughCircles(
-        blurred, cv2.HOUGH_GRADIENT, dp=2, minDist=1000,
-        param1=250, param2=70, minRadius=5, maxRadius=0)
+        # Track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar.
+        def _configure_interior(event):
+            # Update the scrollbars to match the size of the inner frame.
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # Update the canvas's width to fit the inner frame.
+                canvas.config(width=interior.winfo_reqwidth())
+        interior.bind('<Configure>', _configure_interior)
 
-    contrast_value += 10
-    if contrast_value > 255 or circles is None or len(circles[0]) != 1:
-        print("Erreur : Impossible de trouver la lune.")
-        return None
-
-    return circles, image
-
-
-def circle_display_on_image(image, circles):
-    if circles is not None and len(circles[0]) == 1:
-        image_with_circles = image.copy()
-
-        circles = np.uint16(np.around(circles))
-        for i in circles[0, :]:
-            cv2.circle(image_with_circles, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            cv2.circle(image_with_circles, (i[0], i[1]), 2, (0, 0, 255), 3)
-
-        return image_with_circles
-
-    return None
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # Update the inner frame's width to fill the canvas.
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+        canvas.bind('<Configure>', _configure_canvas)
 
 
-if __name__ == '__main__':
-    root = tk.Tk()
-    root.withdraw()
-    img = filedialog.askopenfilename(title="Sélectionnez une image",
-                                     filetypes=[("Fichiers image", "*.png;*.jpg;*.jpeg")])
+if __name__ == "__main__":
 
-    circles, image = moon_coordinates(img)
-    print("Coordonnées du cercle :", circles)
+    class SampleApp(tk.Tk):
+        def __init__(self, *args, **kwargs):
+            root = tk.Tk.__init__(self, *args, **kwargs)
 
-    final_image = circle_display_on_image(image, circles)
-    cv2.imshow('Final Image', final_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+            self.frame = VerticalScrolledFrame(root)
+            self.frame.pack()
+            self.label = ttk.Label(self, text="Shrink the window to activate the scrollbar.")
+            self.label.pack()
+            buttons = []
+            for i in range(10):
+                buttons.append(ttk.Button(self.frame.interior, text="Button " + str(i)))
+                buttons[-1].pack()
+
+    app = SampleApp()
+    app.mainloop()
